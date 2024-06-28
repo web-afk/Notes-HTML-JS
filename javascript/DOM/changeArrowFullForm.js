5077ffee392d45b823fac38b1b766e29af8f6b3d
enrique2016flores <enrique2016flores@gmail.com>
2024-06-28 03:22:22 +0000

    setting color palette for user

--- javascript/DOM/changeArrowFullForm.js
+++ javascript/DOM/changeArrowFullForm.js
@@ -3,7 +3,7 @@ import { handleClickFastFormArrow } from "../events/clickFastFormArrow.js";
 
 export const handleAllFullFormArrows = arrows => {
     const dashBoardFullForm = $(".dashboard-full-form")
-    if(!dashBoardFullForm.classList.contains("show-block")) return 0;
+    if(dashBoardFullForm.classList.contains("hide-block")) return 0;
 
     [...arrows].forEach(arrow => {
         arrow.addEventListener("click", handleClickFastFormArrow)
--- javascript/events/clickFastFormArrow.js
+++ javascript/events/clickFastFormArrow.js
@@ -1,7 +1,18 @@
 import { $ } from "../dom.js"
 
 export const handleClickFastFormArrow = event => {
-    const target = $(".change", event.target)
-    target.classList.toggle("rotate-up")
-    target.classList.toggle("rotate-down")
+    if(event.target.classList.contains("change")){
+        event.target.classList.toggle("rotate-down")
+        event.target.classList.toggle("rotate-up")
+    }else if(event.target.classList.contains("icon-title")){
+        const parent = event.target.parentElement.parentElement
+        const target = $(".change", parent)
+        target.classList.toggle("rotate-down")
+        target.classList.toggle("rotate-up")
+    }else{
+        const target = $(".change", event.target.parentElement)
+        target.classList.toggle("rotate-down")
+        target.classList.toggle("rotate-up")
+    }
+    //console.log({icon: target, parent: event.target.parentElement})
 }
